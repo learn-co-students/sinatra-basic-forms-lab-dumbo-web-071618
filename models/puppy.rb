c3d2fd6bac9b0139f18a7dfb9c034817fa536bf4
Gabriel-Batista <gabedbatista@gmail.com>
2018-08-07 18:39:27 -0400

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,9 @@
+class Puppy
+    attr_accessor :name, :age, :breed
+
+    def initialize(name=nil, breed=nil, age=nil)
+        @name = name
+        @age = age
+        @breed = breed
+    end
+end
