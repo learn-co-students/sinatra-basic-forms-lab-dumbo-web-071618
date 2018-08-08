464cdc8e31483eb211fe04f773b393ddc1c284cc
Kyle-Neale <kyle.a.neale@gmail.com>
2018-08-08 12:15:11 -0400

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,13 @@
+
+class Puppy
+  attr_accessor :name, :breed, :age
+  PUPPIES = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+
+    PUPPIES << self
+  end
+end
